8cedf87cb2f67fd267ab30d8312b94d2f68f78f8
Sanjyot Yeole <yeolesanjyot@gmail.com>
2023-04-27 18:25:24 +0530

    active-page-js-added

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// Managing active page
+function setActivePage (num) {
+    let list_items = document.getElementsByClassName('btm-nav-li-item');
+    for(let i = 0; i < list_items.length; i++) {
+        if (list_items[i].classList.contains('active')) {
+            list_items[i].classList.remove('active');
+            break;    
+        }
+    }
+    if (!list_items[num].classList.contains('active')) {
+        list_items[num].classList.add('active');
+    }
+}
+
+// Valid number of passangers
+
 function passengerNumber(num) {
     if(Math.abs(num) > 0) {
         return Math.abs(num);
